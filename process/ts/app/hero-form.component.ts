9e97d4bb1e817c622e31cc4e251dc9f47056fffb
asinoai <aron.sinoai@linkonomics.com>
2017-04-20 14:25:14 +0300

    Hiding debug stuff and when reseting using a predefined hero

--- process/ts/app/hero-form.component.ts
+++ process/ts/app/hero-form.component.ts
@@ -9,7 +9,7 @@ import { Hero }    from './hero';
 export class HeroFormComponent {
 
   powers = ['Really Smart', 'Super Flexible',
-            'Super Hot', 'Weather Changer'];
+            'Super Hot', 'Weather Changer', 'Fetch any object at any distance'];
 
   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
@@ -23,7 +23,7 @@ export class HeroFormComponent {
   get diagnostic() { return JSON.stringify(this.model); }
 
   newHero() {
-    this.model = new Hero(42, '', '');
+    this.model = this.skyDog(); //new Hero(42, '', '');
   }
 
   skyDog(): Hero {
